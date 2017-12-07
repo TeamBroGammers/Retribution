d373d1995f39a4daef35b080f5f58e16d12ad698
gilmo013 <32023601+gilmo013@users.noreply.github.com>
2017-12-06 23:28:52 -0800

    Everyone make scene with this

---
+++ Retribution Version Everyone can use to make their own scene/Assets/scripts/Character/Bullet.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof (Rigidbody2D))]
+public class Bullet : MonoBehaviour {
+
+	[SerializeField]
+	private float speed=20;
+	private Rigidbody2D myRigidbody;
+	private Vector2 direction;
+
+	void Start () 
+	{
+		myRigidbody = GetComponent<Rigidbody2D>();
+	}
+	void FixedUpdate()
+	{
+		myRigidbody.velocity=direction*speed;
+		BecomeInvisible();
+	}
+	// Update is called once per frame
+	public void Initialize(Vector2 direction)
+	{
+		this.direction=direction;
+	}
+	void BecomeInvisible()
+	{
+		Destroy(gameObject,3f);
+	}
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if((other.tag == "Enemy"))
+			{	
+				Destroy(gameObject);
+			}
+	if((other.tag == "Player"))
+	{	
+		Destroy(gameObject);
+	}
+	}
+}
---
+++ Retribution Version Everyone can use to make their own scene/Assets/scripts/collectables.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collectables : MonoBehaviour {
+
+	void update()
+	{
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "Player")
+		{
+			Destroy (gameObject);
+		}
+
+	}
+}
