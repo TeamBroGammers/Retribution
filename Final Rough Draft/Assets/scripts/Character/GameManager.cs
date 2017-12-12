07731e09892124416991d22588b132bf5e55ea7b
gilmo013 <32023601+gilmo013@users.noreply.github.com>
2017-12-11 23:04:26 -0800

    Final Rough Draft
    All enemies have been created....just drag and drop the enemies you want

---
+++ Final Rough Draft/Assets/scripts/Character/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour{
+	private static GameManager instance;
+	[SerializeField] 
+	private GameObject energyPrefab;
+
+	public GameObject EnergyPrefab {
+		get {
+			return energyPrefab;
+		}
+
+	}
+		public static GameManager Instance {
+		get {
+			if (instance == null) 
+			{
+				instance = FindObjectOfType<GameManager> ();
+			}
+			return instance;
+		}
+	}
+}
