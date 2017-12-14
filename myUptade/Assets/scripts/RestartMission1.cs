0f7884f5ebc9d8866ae3d8bc2416e154c39d0b4f
Romario Mendoza <mendozaromario88@gmail.com>
2017-12-14 13:32:51 -0800

    12/14/17

---
+++ myUptade/Assets/scripts/RestartMission1.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class RestartMission1 : MonoBehaviour {
+
+	public Button Restart;
+
+	void Start()
+	{
+		Button btn = Restart.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+	}
+
+	void TaskOnClick()
+	{
+		SceneManager.LoadScene ("Mission1");
+	}
+
+
+}
